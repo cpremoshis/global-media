e0a50b6fe514b78446e212e59e01328f13756715
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-01-10 21:40:18 -0500

    -

--- app.py
+++ app.py
@@ -224,12 +224,12 @@ with st.sidebar:
             if download_select.endswith(".mp3"):
                 with open(download_select, 'rb') as f:
                     file_name = download_select.split("/")[2]
-                    dwnbtn = st.download_button("Download", type='primary', data=f, file_name=file_name, mime="audio/mpeg")
+                dwnbtn = st.download_button("Download", type='primary', data=f, file_name=file_name, mime="audio/mpeg")
             
             else:                        
                 with open(download_select, 'rb') as f:
                     file_name = download_select.split("/")[2]
-                    dwnbtn = st.download_button("Download", type='primary', data=f, file_name=file_name, mime="video/mp4")
+                dwnbtn = st.download_button("Download", type='primary', data=f, file_name=file_name, mime="video/mp4")
 
     else:
         selections = st.multiselect("Select outlets:", broadcasters_df['Name'], max_selections=4)
