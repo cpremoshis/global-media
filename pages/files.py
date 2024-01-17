5ab3bc00bc63189e1892ba9895b7c61f6690bcb2
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-01-16 23:57:40 -0500

    -

--- pages/files.py
+++ pages/files.py
@@ -3,9 +3,3 @@ import os
 
 path = "/mount/src/global-media/Recordings"
 st.write(os.listdir(path))
-
-with open("/mount/src/global-media/Recordings/files_list.txt", 'r') as f:
-    doc = f.read()
-
-st.write(doc)
-st.write(len(doc))
