12531d59f0ce8549b1c1f79fc1e40f470fb8102d
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-01-22 21:30:56 -0500

    -

--- recording.py
+++ recording.py
@@ -459,11 +459,9 @@ def combine_videos_ffmpeg(video_dict, output_path):
 
         command.extend(['-c:v', 'libx264', '-c:a', 'aac', '-c:s', 'mov_text', output_path])
 
-        return command
+        subprocess.run(" ".join(command), shell=True)
 
-        #subprocess.run(command)
-
-        #return True
+        return True
     
     except Exception as e:
         return e
