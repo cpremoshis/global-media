a3d5618f8306998cb4aaba0f04d4c3c49f034689
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-02-12 11:50:44 -0500

    -

--- pages/files.py
+++ pages/files.py
@@ -1,8 +1,37 @@
 import streamlit as st
 import os
+import time
+import zipfile
 
 path = "/mount/src/global-media/Recordings"
 files = os.listdir(path)
 
 st.write(files)
 
+selections = st.multiselect("Select files:", files)
+
+def zip_files(selections):
+    video_dict = {}
+    for item in selections:
+        if item[0] == True:
+            video_dict[item[1]] = {"Video":item[2], "Subtitles":"None"}
+
+    values = [value for key in video_dict for value in video_dict[key].values()]
+
+    files_to_zip = [item for item in values if item != "None"]
+
+    now = time.time()
+    zip_folder_name = f"./Recordings/download_{now}.zip"
+
+    with zipfile.ZipFile(zip_folder_name, 'w') as zipf:
+        for file in files_to_zip:
+            file_name = file.split("/")[2]
+            zipf.write(file, arcname=file_name)
+
+    return zip_folder_name
+
+if st.download_button("Zip for download"):
+    zip_folder = zip_files(files)
+    file_name = os.path.basename(zip_folder)
+
+dwnbtn = st.download_button("Download", data=zip_folder, file_name=file_name, mime="applicatioin/zip")
