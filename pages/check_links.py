b7a01890ae0c280e1eb8d4626fee4d23237d643f
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-04-06 21:09:17 -0400

    -

--- pages/check_links.py
+++ pages/check_links.py
@@ -6,4 +6,9 @@ with open("./Assets/broadcasters.csv") as file:
     df = pd.read_csv(file)
 
 mpd_links = df[df['Format'] == "MPD"]['Playback M3U8']
+m3u8_play_links = df[df['Format'] == "M3U8"]['Playback M3U8']
+youtube_links = df[df['Format'] == "YouTube"]['Playback M3U8']
+
 st.write(mpd_links)
+st.write(m3u8_play_links)
+st.write(youtube_links)
