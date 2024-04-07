43c520530b97eb196881c1157979343dff69bde1
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-04-06 23:06:33 -0400

    -

--- pages/check_links.py
+++ pages/check_links.py
@@ -93,9 +93,9 @@ if st.button("Check links"):
 
     status = st.empty()
 
-    status.status("Checking PLAYBACK statuses")
+    status.spinner("Checking PLAYBACK statuses")
     df['Playback Status'] = df.apply(check_playback_links, axis=1)
-    status.status("Checking RECORD statuses")
+    status.spinner("Checking RECORD statuses")
     df['Record Status'] = df.apply(check_record_links, axis=1)
 
     st.write(df[['Name', 'Format', 'Record Status']])
