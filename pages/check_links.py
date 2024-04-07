a4ac4777b3a96d051140f7ef19134789677f5288
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-04-06 21:19:45 -0400

    -

--- pages/check_links.py
+++ pages/check_links.py
@@ -2,13 +2,17 @@ import streamlit as st
 import requests
 import pandas as pd
 
+def check_links(row):
+    if row['Format'] == "M3U8":
+        link = row['Playback M3U8']
+        response = requests.get(link)
+        status = response.status_code
+
+        return status
+    
 with open("./Assets/broadcasters.csv") as file:
     df = pd.read_csv(file)
 
-mpd_links = df[df['Format'] == "MPD"]['Playback M3U8']
-m3u8_play_links = df[df['Format'] == "M3U8"]['Playback M3U8']
-youtube_links = df[df['Format'] == "YouTube"]['Playback M3U8']
+df['Status'] = df.apply(check_links)
 
-st.write(mpd_links)
-st.write(m3u8_play_links)
-st.write(youtube_links)
+st.write(df['Name', 'Status'])
