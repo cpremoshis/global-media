6c79467d9b29203a85b99dee0ef3b622e782bcfa
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-04-09 18:59:41 -0400

    -

--- app.py
+++ app.py
@@ -833,7 +833,7 @@ elif display_type == "Upload":
 
     uploaded_file = st.file_uploader("Select file")
 
-    if "processed" not in st.session_state:
+    if st.session_state.processed == False:
         translation_selection = st.radio(
             "Select translation format:",
             ["Subtitles", "Plain text"],
