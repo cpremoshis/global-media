d590e3581201d39cffea107bc593e507625807d9
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-04-09 21:29:20 -0400

    -

--- app.py
+++ app.py
@@ -922,14 +922,14 @@ elif display_type == "Upload":
 
         with status.container():
             st.warning("Please double-check accuracy before use. Automated translation by OpenAI's Whisper.")
-                with status.container():
-                    st.download_button(
-                        label="Download translation",
-                        data=file_to_download,
-                        file_name=st.session_state.download_file_name,
-                        mime='text/plain'
-                        )
-                    if translation_format == "srt":
-                        st.text(content_to_display)
-                    elif translation_format == "text":
-                        st.write(content_to_display)
+            with status.container():
+                st.download_button(
+                    label="Download translation",
+                    data=file_to_download,
+                    file_name=st.session_state.download_file_name,
+                    mime='text/plain'
+                    )
+                if translation_format == "srt":
+                    st.text(content_to_display)
+                elif translation_format == "text":
+                    st.write(content_to_display)
