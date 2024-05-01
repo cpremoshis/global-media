79809a18095d7737ba2338f7247e906f001b0f0b
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-05-01 08:42:02 -0400

    -

--- app.py
+++ app.py
@@ -807,7 +807,7 @@ elif display_type == "CCTV 13 Live":
 
     if stream_status[0] == False:
         st.image("Assets/offline_3.png")
-        st.error(f"Stream ended at {stream_status[1]} GMT")
+        #st.error(f"Stream ended at {stream_status[1]} GMT")
     elif stream_status[0] == True:
         components.html(hls_js_player_html, height=525)
         st.success("Stream status: Live")
