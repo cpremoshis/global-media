a09ed7bcf50477459134d5ee4a9f2a71ccecc5ac
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-05-26 13:28:57 -0400

    -

--- recording.py
+++ recording.py
@@ -560,6 +560,7 @@ def download_from_webpages(link, translate):
             download_command = [
                 'yt-dlp',
                 '--cookies', '/mount/src/global-media/Assets/all_cookies.txt',
+                '--user-agent', '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"'
                 '-o', download_file_path,
                 link_lower
                 ]
