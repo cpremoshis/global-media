aba88e978c9492f5d0d8d32b5430cc269d39ca85
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-05-26 20:13:55 -0400

    -

--- app.py
+++ app.py
@@ -981,7 +981,7 @@ elif tool_type == "Social Media Download":
 
         submitted = st.form_submit_button("Download")
 
-    st.caption("*If downloading from Facebook, do not use the short-code URL (ie: fb.watch/...), use the full-length URL (ie: facebook.com/watch/...)")
+    st.caption("*If downloading from Facebook, use the full-length URL (https://www.facebook.com/watch/...) rather than shortened URL (https://fb.watch/...).")
 
     if submitted and link is not None:
 
