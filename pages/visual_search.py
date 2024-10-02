f4ed2ed7aef8e4b91530c8e0a27adefe1ebefbdd
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2024-10-01 20:52:08 -0400

    -

--- pages/visual_search.py
+++ pages/visual_search.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 
 screenshot = 'https://www.aljazeera.com/wp-content/uploads/2024/10/iran-tel-aviv-israel-missiles-1727802640.jpg?resize=1920%2C1280&quality=80'
 
-openai.api_key = st.secrets['openai_key']
+client = OpenAI(api_key=st.secrets['openai_key'])
 
-response = openai.chat.completions.create(
+response = client.chat.completions.create(
     model='gpt-4o-mini',
     messages={
         'role':'user',
